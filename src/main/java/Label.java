78f6fb35c8be1d9586bc087c8d4c4edcf4063688
andremartingo <andre.martingo@gmail.com>
2018-05-07 14:32:51 +0100

    create interface to get and set labels

--- src/main/java/Label.java
+++ src/main/java/Label.java
@@ -1,5 +1,21 @@
-package PACKAGE_NAME;
-
 public interface Label {
+
+  /**
+   * Returns the label at the [x, y] location
+   *
+   * @param x column index
+   * @param y row index
+   * @return the label at the specified location
+   */
+  int get(int y, int x);
+
+  /**
+   * Updates the label at the [x, y] location
+   *
+   * @param x column index
+   * @param y row index
+   * @param label the new label for the specified location
+   */
+  void set(int y, int x, int label);
 
 }
