6e269a8a9773c2238280c96dfe74f57071488d5d
andremartingo <andre.martingo@gmail.com>
2018-05-07 14:33:59 +0100

    #1 refactor Main to use Model and Deprecate Brain

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.util.List;
+import javax.jws.WebParam.Mode;
 import utils.FileManager;
 import utils.Importer;
 
@@ -22,43 +23,34 @@ public class Main {
 
   public static void main(String[] args) {
 
-//    //Protects the program to have an Array Out of Bounds Exception if nothing is passed as argument
-//    if (args.length > 0) {
-//      //Set the Path to the file to be read on the Program Run Command
-//      String path = args[0];
-//      //Convert JSON file to String
-//      FileManager fileManager = new FileManager();
-//      List<String> listMatrix = fileManager.readFile(path);
-//      System.out.println(
-//          "Input File Processed!!\n");
-//      //Convert String to Java Matrix
-//      Importer importer = new Importer();
-//      int[][] matrix = importer.importMatrix(listMatrix);
-//      //Calculate Adjacent Cells
-//      Brain brain = new Brain(matrix);
-//      List<List<Point>> result = brain.calculate();
-//      printList(result);
-//    } else {
-//      System.out.println("Please set the path for the Input File as Program Argument");
-//    }
-    int[][] matrix2 = new int[5][5];
-    for (int row =0; row < matrix2.length; row++){
-      for(int col=0 ; col < matrix2.length; col++){
-        matrix2[row][col] = 1;
-      }
+    //Protects the program to have an Array Out of Bounds Exception if nothing is passed as argument
+    if (args.length > 0) {
+      //Set the Path to the file to be read on the Program Run Command
+      String path = args[0];
+      //Convert JSON file to String
+      FileManager fileManager = new FileManager();
+      List<String> listMatrix = fileManager.readFile(path);
+      System.out.println(
+          "Input File Processed!!\n");
+      //Convert String to Java Matrix
+      Importer importer = new Importer();
+      int[][] matrix = importer.importMatrix(listMatrix);
+      //Calculate Adjacent Cells
+      Model brain = new Model(matrix);
+      List<List<Point>> result = brain.labelMatrix();
+      result.toString();
+    } else {
+      System.out.println("Please set the path for the Input File as Program Argument");
     }
-
-    Model model = new Model(matrix);
-    model.labelMatrix();
-    model.print();
-    model.printHashmap();
-    model.printResult();
+    Model brain = new Model(matrix);
+    List<List<Point>> result = brain.labelMatrix();
+    printList(result);
 
   }
 
-    private static void printList(List<List<Point>> result) {
+  private static void printList(List<List<Point>> result) {
     for (List<Point> list : result) {
-      System.out.println(list);
+      System.out.print(list);
       System.out.println("\n");
     }
   }
